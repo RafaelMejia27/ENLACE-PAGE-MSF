cb518825519d2d9cd625334a0e9b85ab3ecf7ee7
Ismael2511 <ismaeldso125@gmail.com>
2020-11-03 09:39:14 -0400

    INITIAL COMMIT
    Comienzo de proyecto

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!--img alt="Vue logo" src="./assets/logo.png"-->
+    <Register/>
+  </div>
+</template>
+
+<script>
+import Register from './components/Register'
+
+export default {
+  name: 'App',
+  components: {
+    Register
+  }
+}
+</script>
+
+<!--style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style-->
---
+++ src/components/Register.vue
@@ -0,0 +1,36 @@
+<template>
+        <div class="main-sign">
+           
+            <div class="container-input">
+                <div class="logo">
+                    <img src="img/logo.png" alt="Logo Pic">
+                </div>
+
+                <div class="sign-inp">
+                    <p class="clr-darkBlue fnt-medium">Razón Social</p>
+                    <input type="text" class="input-field">
+                </div>
+                <div class="sign-inp">
+                    <p class="clr-darkBlue fnt-medium">RNC</p>
+                    <input type="text" class="input-field">
+                </div>
+                <div class="sign-inp">
+                    <p class="clr-darkBlue fnt-medium">Nombre de Usuario</p>
+                    <input type="text" class="input-field">
+                </div>
+                <div class="sign-inp">
+                    <p class="clr-darkBlue fnt-medium">Contraseña</p>
+                    <input type="password" class="input-field">
+                </div>
+      
+                <div class="check-valid">
+                    <input type="checkbox" class="input-chkd" id=""><span class="clr-gray">Acepto las politicas que se describen <a href="#">aquí</a></span>
+                </div>    
+                <input type="submit" value="Registrar" class="btn btn-blue" />
+            </div>   
+
+            <div class="sign-pic">
+                <img src="img/gov.jpg" alt="gov image">
+            </div>
+        </div>
+</template>
